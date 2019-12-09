a18068f811bbae008a248b02f0c5a86c50f1ecba
André Cruz <andremiguelcruz@msn.com>
2019-12-09 01:42:18 +0000

    docs: add link to private recipes
    Also:
    
    - Lowercase "Browser Support" for consistency with other docs
    - Let the header logo & title occupy the whole width when in tablet/mobile

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -32,6 +32,11 @@ module.exports = {
             label: 'Recipes',
             items: [
                 'recipes/api/recipe-api',
+                {
+                    type: 'link',
+                    label: 'Private recipes',
+                    href: 'https://next-with-moxy-recipes.moxy.tech',
+                },
             ],
         },
         {
