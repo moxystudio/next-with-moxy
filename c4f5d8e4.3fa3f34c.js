cafb4b7ee848e31cb6939eb08d4843b5a38faf99
satazor <satazor@users.noreply.github.com>
2020-01-09 02:54:59 +0000

    Automated deployment: Thu Jan  9 02:54:59 UTC 2020 3f780f63b5f5170170017582fc11233ec6151e72

---
+++ c4f5d8e4.3fa3f34c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{142:function(t,n,e){"use strict";e.r(n);var c=e(0),o=e(151);n.default=function(){var t=Object(o.a)().siteConfig;return Object(c.useEffect)((function(){window.location=t.baseUrl+"docs/welcome/what-is-this"}),[t]),null}},151:function(t,n,e){"use strict";var c=e(0),o=e(51);n.a=function(){return Object(c.useContext)(o.a)}}}]);
