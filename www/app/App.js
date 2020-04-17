5c50ab1fa2da33224f1a09ac9a2f02ca6eb8637e
João Peixoto <34691925+jdiogopeixoto@users.noreply.github.com>
2020-04-17 10:14:57 +0100

    feat: add next-seo (#89)

--- www/app/App.js
+++ www/app/App.js
@@ -4,11 +4,12 @@ import Head from 'next/head';
 import KeyboardOnlyOutlines from '@moxy/react-keyboard-only-outlines';
 import { withNextIntlSetup } from '@moxy/next-intl';
 import { LayoutTree } from '@moxy/next-layout';
+import Seo from '@moxy/next-seo';
 import { CookiesProvider } from 'react-cookie';
 import nextIntlConfig from '../../intl';
 import { MainLayout, CookieBanner } from '../shared/components';
 import { initGTM, destroyGTM } from '../shared/utils/google-tag-manager';
-import SEO_DATA from './App.data.js';
+import { seoData } from './App.data.js';
 
 export const App = ({ Component, pageProps }) => {
     const handleCookieConsents = useCallback((cookieConsents) => {
@@ -22,9 +23,6 @@ export const App = ({ Component, pageProps }) => {
     return (
         <CookiesProvider>
             <Head>
-                <title>{ SEO_DATA.title }</title>
-                <meta name="description" content={ SEO_DATA.description } />
-                <meta name="keywords" content={ SEO_DATA.keywords.join(' ') } />
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png?v=M4KN2GElyG" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png?v=M4KN2GElyG" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png?v=M4KN2GElyG" />
@@ -36,22 +34,8 @@ export const App = ({ Component, pageProps }) => {
                 <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta name="msapplication-config" content="/favicons/browserconfig.xml?v=M4KN2GElyG" />
                 <meta name="theme-color" content="#ffffff" />
-                {/* Facebook & search engines */}
-                <meta property="og:url" content={ SEO_DATA.url } />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content={ SEO_DATA.title } />
-                <meta property="og:description" content={ SEO_DATA.description } />
-                <meta property="og:site_name" content={ SEO_DATA.title } />
-                <meta property="og:image" content={ SEO_DATA.image.src } />
-                <meta property="og:image:width" content={ SEO_DATA.image.width } />
-                <meta property="og:image:height" content={ SEO_DATA.image.height } />
-                {/* Twitter */}
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content={ SEO_DATA.url } />
-                <meta property="twitter:title" content={ SEO_DATA.title } />
-                <meta property="twitter:description" content={ SEO_DATA.description } />
-                <meta property="twitter:image" content={ SEO_DATA.image.src } />
             </Head>
+            <Seo data={ seoData } />
 
             <KeyboardOnlyOutlines />
             <CookieBanner onCookieConsents={ handleCookieConsents } />
