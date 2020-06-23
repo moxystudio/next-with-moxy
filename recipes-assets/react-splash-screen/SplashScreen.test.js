cc26da1f461007ccf759e9e1af33fc1a1700e1bf
ruimonteiro93 <ruimonteiro93@users.noreply.github.com>
2020-06-23 12:51:39 +0000

    Automated deployment: Tue Jun 23 12:51:39 UTC 2020 da8562af01f00fe2b6a029b473b5cdae1dd23470

--- recipes-assets/react-splash-screen/SplashScreen.test.js
+++ recipes-assets/react-splash-screen/SplashScreen.test.js
@@ -1,50 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import WaitForReact from '@moxy/react-wait-for-react';
 import SplashScreen from './SplashScreen';
-import AppTree from '../../test-utils/modules/react-app-tree';
+import { render, screen } from 'test-utils';
 
 jest.mock('@moxy/react-wait-for-react', () => jest.fn(() => null));
 
 it('should render the content and progress bar correctly when progress is 0', () => {
     WaitForReact.mockImplementationOnce(({ children }) => children({ progress: 0, error: undefined }));
 
-    const { container } = render(<AppTree><SplashScreen /></AppTree>);
+    const { getByDataWaitForReactElement } = render(<SplashScreen />);
 
-    expect(container).not.toHaveTextContent('error');
-    expect(container.querySelector('[data-wait-for-react-element="progressBar"]')).toHaveStyle({ transform: 'scaleX(0)' });
-    expect(container.querySelector('[data-wait-for-react-element="splashScreen"]')).not.toHaveClass('loading', 'loaded');
+    expect(screen.getByText('error')).not.toBeInTheDocument();
+    expect(getByDataWaitForReactElement('progressBar')).toHaveStyle({ transform: 'scaleX(0)' });
+    expect(getByDataWaitForReactElement('splashScreen')).not.toHaveClass('loading', 'loaded');
 });
 
 it('should render the content and progress bar correctly when progress is between 0 and 1', () => {
     WaitForReact.mockImplementationOnce(({ children }) => children({ progress: 0.2, error: undefined }));
 
-    const { container } = render(<AppTree><SplashScreen /></AppTree>);
+    const { getByDataWaitForReactElement } = render(<SplashScreen />);
 
-    expect(container).not.toHaveTextContent('error');
-    expect(container.querySelector('[data-wait-for-react-element="progressBar"]')).toHaveStyle({ transform: 'scaleX(0.2)' });
-    expect(container.querySelector('[data-wait-for-react-element="splashScreen"]')).toHaveClass('loading');
-    expect(container.querySelector('[data-wait-for-react-element="splashScreen"]')).not.toHaveClass('loaded');
+    expect(screen.getByText('error')).not.toBeInTheDocument();
+    expect(getByDataWaitForReactElement('progressBar')).toHaveStyle({ transform: 'scaleX(0.2)' });
+    expect(getByDataWaitForReactElement('splashScreen')).toHaveClass('loading');
+    expect(getByDataWaitForReactElement('splashScreen')).not.toHaveClass('loaded');
 });
 
 it('should render the content and progress bar correctly when progress is 1', () => {
     WaitForReact.mockImplementationOnce(({ children }) => children({ progress: 1, error: undefined }));
 
-    const { container } = render(<AppTree><SplashScreen /></AppTree>);
+    const { getByDataWaitForReactElement } = render(<SplashScreen />);
 
-    expect(container).not.toHaveTextContent('error');
-    expect(container.querySelector('[data-wait-for-react-element="progressBar"]')).toHaveStyle({ transform: 'scaleX(1)' });
-    expect(container.querySelector('[data-wait-for-react-element="splashScreen"]')).toHaveClass('loaded');
-    expect(container.querySelector('[data-wait-for-react-element="splashScreen"]')).not.toHaveClass('loading');
+    expect(screen.getByText('error')).not.toBeInTheDocument();
+    expect(getByDataWaitForReactElement('progressBar')).toHaveStyle({ transform: 'scaleX(1)' });
+    expect(getByDataWaitForReactElement('splashScreen')).toHaveClass('loaded');
+    expect(getByDataWaitForReactElement('splashScreen')).not.toHaveClass('loading');
 });
 
 it('should render an error if the promise failed', () => {
     WaitForReact.mockImplementationOnce(({ children }) => children({ progress: 0.2, error: new Error('foo') }));
 
-    const { container } = render(<AppTree><SplashScreen /></AppTree>);
+    const { getByDataWaitForReactElement } = render(<SplashScreen />);
 
-    expect(container).toHaveTextContent('error');
-    expect(container.querySelector('[data-wait-for-react-element="progressBar"]')).toHaveStyle({ transform: 'scaleX(0.2)' });
-    expect(container.querySelector('[data-wait-for-react-element="splashScreen"]')).toHaveClass('loading');
-    expect(container.querySelector('[data-wait-for-react-element="splashScreen"]')).not.toHaveClass('loaded');
+    expect(screen.getByText('error')).toBeInTheDocument();
+    expect(getByDataWaitForReactElement('progressBar')).toHaveStyle({ transform: 'scaleX(0.2)' });
+    expect(getByDataWaitForReactElement('splashScreen')).toHaveClass('loading');
+    expect(getByDataWaitForReactElement('splashScreen')).not.toHaveClass('loaded');
 });
