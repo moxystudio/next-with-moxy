098fbb09a4071db48bdaa195194ec3145004046d
André Cruz <andremiguelcruz@msn.com>
2020-08-27 02:55:12 +0100

    chore: minor comment improvement

--- www/shared/modules/next-router/use-page-router.js
+++ www/shared/modules/next-router/use-page-router.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { useRouter } from 'next/router'; // eslint-disable-line no-restricted-imports
 import usePageKey from './use-page-key';
 
-// You should use this hook instead of the one provided by Next.js because it
+// You should use this hook instead of useRouter() provided by Next.js because it
 // returns the correct router even during page transitions. During the transition,
 // the old page will still be mounted when animating out, but the router has already changed.
 // This hook returns the same router your page started with.
