302b1c29d85e853fcd55e4d62b3e2d2b41e84677
André Cruz <andremiguelcruz@msn.com>
2021-05-26 14:02:38 +0100

    feat: add new standardize typography styles

--- www/pages/home/Home.js
+++ www/pages/home/Home.js
@@ -11,6 +11,23 @@ const Home = () => (
             <h1>
                 <FormattedMessage id="home.title" />
             </h1>
+
+            <div className={ styles.typographyShowcase }>
+                <h1>typography-h1</h1>
+                <h2>typography-h2</h2>
+                <h3>typography-h3</h3>
+                <h4>typography-h4</h4>
+                <h5>typography-h5</h5>
+                <h6>typography-h6</h6>
+
+                <div className={ styles.subtitle1 }>typography-subtitle-1</div>
+                <div className={ styles.subtitle2 }>typography-subtitle-2</div>
+                <div className={ styles.body1 }>typography-body-1</div>
+                <div className={ styles.body2 }>typography-body-2</div>
+                <div className={ styles.textButton }>typography-text-button</div>
+                <div className={ styles.caption }>typography-caption</div>
+                <div className={ styles.overline }>typography-overline</div>
+            </div>
         </Container>
     </main>
 );
